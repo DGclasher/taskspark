1260751b3c06b8606e3f5cfdec1a24c2cdb74e57
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-08-20 23:44:20 +0530

    deployment

--- migrate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /app/ || exit
-
-/opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py collectstatic --noinput
-/opt/venv/bin/python manage.py createsuperuser --noinput || true
